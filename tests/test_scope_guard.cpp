d23b76298b3abf4af648d3439645389adb035888
Edouard Gomez <ed.gomez@free.fr>
2026-02-19 01:49:01 +0100

    tests: add more scope guard tests

--- tests/test_scope_guard.cpp
+++ tests/test_scope_guard.cpp
@@ -9,7 +9,9 @@
 
 #include <egomez/vulkan_tutorial/scope_guard.h>
 
+#include <functional>
 #include <stdexcept>
+#include <vector>
 
 TEST(ScopeGuard, NormalOutOfScopeLambda)
 {
@@ -98,4 +100,50 @@ TEST(ScopeGuard, DismissExceptionOutOfScope)
         exception_caught = true;
     }
     ASSERT_TRUE(exception_caught);
+}
+
+TEST(ScopeGuard, UnnamedMacroExecutesOnScopeExit)
+{
+    bool called = false;
+    {
+        SCOPE_GUARD([&called] { called = true; });
+        ASSERT_FALSE(called);
+    }
+    ASSERT_TRUE(called);
+}
+
+TEST(ScopeGuard, MultipleGuardsRunInReverseOrder)
+{
+    std::vector<int> call_order;
+    {
+        SCOPE_GUARD([&call_order] { call_order.push_back(1); });
+        SCOPE_GUARD([&call_order] { call_order.push_back(2); });
+        SCOPE_GUARD([&call_order] { call_order.push_back(3); });
+    }
+    ASSERT_EQ(call_order.size(), 3u);
+    EXPECT_EQ(call_order[0], 3);
+    EXPECT_EQ(call_order[1], 2);
+    EXPECT_EQ(call_order[2], 1);
+}
+
+TEST(ScopeGuard, DismissingOneGuardDoesNotAffectOthers)
+{
+    int called_count = 0;
+    {
+        SCOPE_GUARD_NAMED(first, [&called_count] { ++called_count; });
+        SCOPE_GUARD_NAMED(second, [&called_count] { ++called_count; });
+        second.dismiss();
+    }
+    ASSERT_EQ(called_count, 1);
+}
+
+TEST(ScopeGuard, DismissCanBeCalledMultipleTimes)
+{
+    bool called = false;
+    {
+        SCOPE_GUARD_NAMED(guard, [&called] { called = true; });
+        guard.dismiss();
+        guard.dismiss();
+    }
+    ASSERT_FALSE(called);
 }
